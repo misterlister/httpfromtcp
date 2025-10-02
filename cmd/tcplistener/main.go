6ea060458c82ed1c29fce2dc0f3f15ab56f58eab
misterlister <haydenglister@gmail.com>
2025-10-02 13:53:23 -0700

    Updated main to work with new http parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
+	"log"
 	"net"
-	"strings"
+
+	"github.com/misterlister/httpfromtcp/internal/request"
 )
 
 func main() {
@@ -22,57 +22,19 @@ func main() {
 		conn, err := listener.Accept()
 
 		if err != nil {
-			fmt.Println(err)
-			continue
+			log.Fatalf("error: %s\n", err.Error())
 		}
 
 		fmt.Println("A connection has been accepted!")
 
-		lines := getLinesChannel(conn)
-
-		for line := range lines {
-			fmt.Printf("%s\n", line)
+		requestLine, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("error parsing request: %s\n", err.Error())
 		}
 
-		conn.Close()
-		fmt.Println("The connection has been closed!")
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", requestLine.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", requestLine.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", requestLine.RequestLine.HttpVersion)
 	}
-
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	ch := make(chan string)
-	go func() {
-
-		var currentLine string
-
-		data := make([]byte, 8)
-
-		defer close(ch)
-		for {
-			bytesRead, err := f.Read(data)
-
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					if currentLine != "" {
-						ch <- currentLine
-					}
-				} else {
-					fmt.Printf("error: %s\n", err)
-				}
-				break
-			}
-
-			parts := strings.Split(string(data[:bytesRead]), "\n")
-
-			for i := 0; i < len(parts)-1; i++ {
-				currentLine += parts[i]
-				ch <- currentLine
-				currentLine = ""
-			}
-
-			currentLine += parts[len(parts)-1]
-		}
-	}()
-	return ch
 }
