08c7d65cae6cad7fe82d026169800afe76084b4f
misterlister <haydenglister@gmail.com>
2025-10-03 14:07:52 -0700

    Created tests for header parse module

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,65 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/misterlister/httpfromtcp/internal/request"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaderLineParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid separator header
+	headers = NewHeaders()
+	data = []byte("Code;42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Valid header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("       Host: localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 37, n)
+	assert.False(t, done)
+
+	// Test: Valid pair of headers
+	headers = NewHeaders()
+	data = []byte("Host:localhost:42069\r\nNumber:123\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 22, n)
+	assert.False(t, done)
+	n, done, err = headers.Parse(data[n:])
+	require.NoError(t, err)
+	assert.Equal(t, 12, n)
+	assert.False(t, done)
+
+	// Test: Valid done
+	headers = NewHeaders()
+	data = []byte("\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, len(request.Crlf), n)
+	assert.True(t, done)
+}
