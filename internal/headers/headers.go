a50c715c7f5aa48655dd1acf52807d48cf6b7d48
misterlister <haydenglister@gmail.com>
2025-10-07 12:18:36 -0700

    Updated header keys to be case-insensitive, and added errors for invalid characters

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"strings"
+	"unicode"
 
 	"github.com/misterlister/httpfromtcp/internal/request"
 )
@@ -12,6 +13,24 @@ type Headers map[string]string
 
 func NewHeaders() Headers {
 	return make(Headers)
+}
+
+var ValidHeaderCharacters = map[rune]bool{
+	'!':  true,
+	'#':  true,
+	'$':  true,
+	'%':  true,
+	'&':  true,
+	'\'': true,
+	'*':  true,
+	'+':  true,
+	'-':  true,
+	'.':  true,
+	'^':  true,
+	'_':  true,
+	'`':  true,
+	'|':  true,
+	'~':  true,
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
@@ -29,14 +48,37 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 0, false, fmt.Errorf("error: invalid key:value pair. '%s' doesn't contain the ':' separator", string(data))
 	}
 
-	key := string(data[:sep])
-	val := string(data[sep+1 : endl])
+	keyString := string(data[:sep])
+	valString := string(data[sep+1 : endl])
 
-	if key[sep-1] == ' ' {
-		return 0, false, fmt.Errorf("error: invalid key. '%s' ends in whitespace", key)
+	if keyString[sep-1] == ' ' {
+		return 0, false, fmt.Errorf("error: invalid key. '%s' ends in whitespace", keyString)
 	}
 
-	h[strings.TrimSpace(key)] = strings.TrimSpace(val)
+	key := strings.ToLower(strings.TrimSpace(keyString))
+	if !isValidHeaderString(key) {
+		return 0, false, fmt.Errorf("error: invalid key. '%s' contains invalid characters", key)
+	}
+
+	val := strings.ToLower(strings.TrimSpace(valString))
+
+	h[key] = val
 
 	return endl + len(request.Crlf), false, nil
+}
+
+func isValidHeaderString(s string) bool {
+	for _, r := range s {
+		switch {
+		case unicode.IsLetter(r):
+			continue
+		case unicode.IsDigit(r):
+			continue
+		case ValidHeaderCharacters[r]:
+			continue
+		default:
+			return false
+		}
+	}
+	return true
 }
